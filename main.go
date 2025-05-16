72080f93984165973ab5e6f4790ba3da3b117aca
Murilo Cruz <murilomecr@gmail.com>
2025-05-16 17:12:25 -0300

    adding SES integration

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ses"
+	"github.com/aws/aws-sdk-go-v2/service/ses/types"
 )
 
 var (
@@ -23,6 +26,34 @@ func handleTriggering(ctx context.Context, s3Event events.S3Event) error {
 			s3.Object.Key,
 			s3.Object.Size,
 		)
+
+		subject := fmt.Sprintf("Object received in the bucket: %s", s3.Bucket.Name)
+		message := fmt.Sprintf("The file name is: %s and the size is %d", s3.Object.Key, s3.Object.Size)
+
+		input := ses.SendEmailInput{
+			Source: aws.String("murilomecr@outlook.com"),
+			Destination: &types.Destination{
+				ToAddresses: []string{"murilomecr@gmail.com"},
+			},
+			Message: &types.Message{
+				Subject: &types.Content{
+					Data: aws.String(subject),
+				},
+				Body: &types.Body{
+					Text: &types.Content{
+						Data: aws.String(message),
+					},
+				},
+			},
+		}
+
+		result, err := sesClient.SendEmail(ctx, &input)
+
+		if err != nil {
+			log.Fatalf("Failed to send email: %v", err)
+		}
+
+		fmt.Println("Email sent successfully, Message ID:", *result.MessageId)
 	}
 	return nil
 }
