8b9c98a1050d48b05e0628ba87e7a8a02e4223ed
Murilo Cruz <murilomecr@gmail.com>
2025-05-16 17:03:53 -0300

    seeing the s3 event correctly

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ses"
@@ -13,9 +14,16 @@ var (
 	sesClient *ses.Client
 )
 
-func handleTriggering(ctx context.Context) error {
-	log.Printf("This is the context: %v", ctx)
-
+func handleTriggering(ctx context.Context, s3Event events.S3Event) error {
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		log.Printf(
+			"S3 event received: bucket=%s, key=%s, size=%d bytes",
+			s3.Bucket.Name,
+			s3.Object.Key,
+			s3.Object.Size,
+		)
+	}
 	return nil
 }
 
